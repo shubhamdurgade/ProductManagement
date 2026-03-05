f50990ff3cbbe35720a65cc226d233e30dc46071
Shubham Durgade <shubhamdurgade1203@gmail.com>
2026-03-06 00:13:36 +0530

    Install EntityFrameworkCore SQL Server and Tools NuGet package Implement Product.cs class for product details

---
+++ Domain/Entites/Product.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace ProductManagement.Domain.Entites
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(100, MinimumLength = 2)]
+        public string Name { get; set; } = null!;
+
+        [StringLength(500)]
+        public string? Description { get; set; }
+
+        [Column(TypeName = "decimal(12,2)")]
+        [Range(0.01, 1000000, ErrorMessage = "Price must be positive and less than 10 lakh.")]
+        public decimal? Price { get; set; }
+
+        [Range(0, 1000, ErrorMessage = "Stack cannot be negative.")]
+        public int Stock { get; set; }
+
+        public void validateBusinessRules()
+        {
+            if (Price <= 0)
+                throw new InvalidOperationException("Product price must be positive.");
+            if (Stock < 0)
+                throw new InvalidOperationException("Product price must be positive.");
+        }
+    }
+}
