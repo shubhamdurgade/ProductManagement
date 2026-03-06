11c6bac928a0bd225b906525404d4195b11b7e53
Shubham Durgade <shubhamdurgade1203@gmail.com>
2026-03-06 23:55:18 +0530

    Add ApplicationDbContext for EF Core database operations Define IProductRepository interface for product data access functionality

---
+++ Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using ProductManagement.Domain.Entites;
+using System.Collections.Generic;
+
+namespace ProductManagement.Domain.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllAsync();
+
+        Task<Product?> GetByIdAsync(int id);
+
+        Task<Product?> AddAsync(Product product);
+
+        Task UpdateAsync(Product product);
+
+        Task DeleteAsync(int id);
+    }
+}
---
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductManagement.Domain.Entites;
+
+namespace ProductManagement.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        } 
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product { Id = 1, Name = "Laptop", Description = "High-end gaming laptop"},
+                new Product { Id = 2, Name = "Smartphone", Description = "Latest model smartphone"},
+                new Product { Id = 3 , Name = "Wireless HeadPhone", Description = "Noise cancelation"}
+            );
+        }
+
+        public DbSet<Product> Products { get; set; } = null!;
+    }
+}
