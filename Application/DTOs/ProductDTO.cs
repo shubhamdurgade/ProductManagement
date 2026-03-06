91ca40dffdfa7016859cb8f41bdc1c1388783c76
Shubham Durgade <shubhamdurgade1203@gmail.com>
2026-03-07 00:15:20 +0530

    Implement ProductRepository class implementing IProductRepository Create ProductDTO in Application.DTOs for product data transfer between layers

---
+++ Application/DTOs/ProductDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace ProductManagement.Application.DTOs
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public decimal Price { get; set; }  
+
+        public int Stock {  get; set; }
+    }
+}
---
+++ Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductManagement.Domain.Entites;
+using ProductManagement.Domain.Repositories;
+using ProductManagement.Infrastructure.Data;
+
+namespace ProductManagement.Infrastructure.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<IEnumerable<Product>> GetAllAsync()
+        {
+            return await _context.Products.AsNoTracking().ToListAsync();
+        } 
+
+        public async Task<Product?> GetByIdAsync(int id)
+        {
+            return await _context.Products.AsNoTracking().FirstOrDefaultAsync(prd => prd.Id == id);
+        }
+
+        public async Task<Product?> AddAsync(Product product)
+        {
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if(product != null)
+            {
+                _context.Products.Remove(product);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task UpdateAsync(Product product)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
